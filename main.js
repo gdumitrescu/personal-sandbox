3ff5778c2766a35c5e72ce015631e61b15bd220f
Sandbox Sync <support@getsandbox.com>
2015-05-28 23:47:26 -0400

    Changes

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ Sandbox.define("/skills", "POST", skills.postSkills);
 Sandbox.define("/skills/{id}", "GET", skills.getSkillsById);
 Sandbox.define("/skills/{id}", "PUT", skills.putSkills);
 Sandbox.define("/skills/{id}", "DELETE", skills.deleteSkills);
+
+
+Sandbox.define('/languages','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the Content-Type header.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
