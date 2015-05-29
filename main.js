378a87a36b8acd570769b02cb888f5cc3772cd73
Sandbox Sync <support@getsandbox.com>
2015-05-29 00:11:14 -0400

    Changes

--- main.js
+++ main.js
@@ -305,3 +305,21 @@ Sandbox.define('/badges','GET', function(req, res) {
         "status": "ok"
     });
 })
+
+Sandbox.define('/projects','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the Content-Type header.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
